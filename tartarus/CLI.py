8883cacd3336177b3599aeff1c44aa42a7f690f0
Henry Till <henrytill@gmail.com>
2023-06-06 21:54:22 -0700

    implement a very rough lookup

---
+++ tartarus/CLI.py
@@ -0,0 +1,62 @@
+import argparse
+import os
+from enum import Enum
+
+from tartarus.Data import Entries
+from tartarus.GPG import GPG
+
+
+class Verbosity(Enum):
+    """
+    Represents the verbosity level of the output.
+    """
+
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
+
+
+def lookup(args: argparse.Namespace):
+    """
+    Searches for an 'Entry' that matches the provided description.
+
+    Args:
+        args: The arguments provided by the user.
+    """
+    config_home = os.getenv('XDG_CONFIG_HOME') or '~/.config'
+    data_dir = os.getenv('HECATE_DATA_DIR')
+    if data_dir is None:
+        data_dir = os.path.join(config_home, 'hecate')
+        data_file = os.path.join(data_dir, 'db', 'data.json')
+    else:
+        data_file = os.path.join(data_dir, 'db', 'data.json')
+    # Parse the data file
+    with open(data_file, 'r') as f:
+        data = f.read()
+        entries = Entries.from_json(data)
+    # Lookup the entry
+    entry = entries.lookup(args.description, args.identity)
+    # Print the entry
+    for e in entry:
+        ciphertext = e.ciphertext
+        plaintext = GPG.decrypt(ciphertext)
+        print(plaintext)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='A minimal password manager.')
+    subparsers = parser.add_subparsers(help='Commands')
+
+    parser_lookup = subparsers.add_parser('lookup')
+    parser_lookup.add_argument('description')
+    parser_lookup.add_argument('--identity')
+    parser_lookup.add_argument('--verbosity', default=1, type=Verbosity)
+    parser_lookup.set_defaults(func=lookup)
+
+    args = parser.parse_args()
+
+    # Call the appropriate function based on the provided command
+    if hasattr(args, 'func'):
+        args.func(args)
+    else:
+        parser.print_help()
--- tartarus/Data.py
+++ tartarus/Data.py
@@ -1,12 +1,41 @@
+import json
 from dataclasses import dataclass
 from datetime import datetime
+from enum import Enum
+from pathlib import Path
 from typing import NewType, Optional
-
-Id = NewType('Id', str)
-"""Uniquely identifies an 'Entry'."""
 
 KeyId = NewType('KeyId', str)
 """Represents a GPG Key Id."""
+
+
+class Backend(Enum):
+    """
+    Represents the backend used to store the data.
+    """
+
+    SQLITE = 1
+    JSON = 2
+
+
+@dataclass
+class Config:
+    """
+    A configuration object.
+    """
+
+    data_dir: Path
+    """The directory where the data is stored."""
+    backend: Backend
+    """The backend used to store the data."""
+    gpg_key_id: KeyId
+    """The GPG Key Id used for encryption."""
+    allow_multiple_keys: bool
+    """Whether or not to allow multiple GPG keys to be used for encryption."""
+
+
+Id = NewType('Id', str)
+"""Uniquely identifies an 'Entry'."""
 
 Description = NewType('Description', str)
 """Describes an 'Entry'. Can be a URI or a descriptive name."""
@@ -43,3 +72,107 @@ class Entry:
     identity: Optional[Identity]
     ciphertext: Ciphertext
     meta: Optional[Metadata]
+
+    @classmethod
+    def from_dict(cls, data: dict) -> Optional['Entry']:
+        """
+        Creates an 'Entry' from a dictionary.
+
+        Args:
+            data: The dictionary to create the 'Entry' from.
+
+        Returns:
+            The created 'Entry'.
+        """
+        data = {k.lower(): v for k, v in data.items()}
+        try:
+            return cls(
+                id=Id(data['id']),
+                key_id=KeyId(data['keyid']),
+                timestamp=datetime.fromisoformat(data['timestamp']),
+                description=Description(data['description']),
+                identity=Identity(data['identity']) if 'identity' in data else None,
+                ciphertext=Ciphertext(data['ciphertext']),
+                meta=Metadata(data['meta']) if 'meta' in data else None,
+            )
+        except KeyError:
+            return None
+
+    def to_string(self, verbosity: int) -> str:
+        """
+        Converts the 'Entry' to a string.
+
+        Args:
+            verbosity: The verbosity level of the output.
+
+        Returns:
+            The 'Entry' as a string.
+        """
+        return f'{self.ciphertext}'
+
+
+@dataclass
+class Entries:
+    """
+    A collection of 'Entry' objects.
+
+    Attributes:
+        entries: The collection of entries.
+    """
+
+    entries: list[Entry]
+
+    @classmethod
+    def __from_dicts(cls, data: list[dict]) -> 'Entries':
+        """
+        Creates an 'Entries' object from a list of dictionaries.
+
+        Args:
+            data: The list of dictionaries to create the 'Entries' object from.
+
+        Returns:
+            The created 'Entries' object.
+        """
+        ret = [Entry.from_dict(entry) for entry in data if Entry.from_dict(entry) is not None]
+        return cls(ret)
+
+    @classmethod
+    def from_json(cls, data: str) -> 'Entries':
+        """
+        Creates an 'Entries' object from a JSON string.
+
+        Args:
+            data: The JSON string to create the 'Entries' object from.
+
+        Returns:
+            The created 'Entries' object.
+        """
+        object = json.loads(data)
+        if isinstance(object, list):
+            return cls.__from_dicts(object)
+        else:
+            return cls([])
+
+    def lookup(self, description: Description, identity: Optional[Identity] = None) -> list[Entry]:
+        """
+        Searches for entries that match the provided description and identity.
+
+        Matching is fuzzy and case-insensitive.
+
+        Args:
+            description: The description to search for.
+            identity: Optional identity to search for.
+
+        Returns:
+            A list of entries that match the provided description and identity.
+        """
+        return [
+            entry
+            for entry in self.entries
+            if description.lower() in entry.description.lower()
+            and (identity is None or identity.lower() in entry.identity.lower())
+        ]
+
+
+Plaintext = NewType('Plaintext', str)
+"""Holds a plaintext value."""
