ba818396d17353c01b6d520306cbcdc344660a6b
Henry Till <henrytill@gmail.com>
2023-06-08 00:23:22 -0700

    scripts: tweak docstrings

--- scripts.py
+++ scripts.py
@@ -3,16 +3,14 @@ import subprocess
 
 def test():
     """
-    Run all unittests. Equivalent to:
-    `poetry run python -u -m unittest discover`
+    Run all unittests.
     """
     subprocess.run(['python', '-u', '-m', 'unittest', 'discover'])
 
 
 def test_coverage():
     """
-    Run all unittests with coverage. Equivalent to:
-    `poetry run python -u -m coverage run -m unittest discover`
+    Run all unittests with coverage.
     """
     subprocess.run(['coverage', 'run', '-m', 'unittest', 'discover'])
     subprocess.run(['coverage', 'xml'])
