4a9fbdf4c96d5f4f1254ebc1eab012f99250f60f
Henry Till <henrytill@gmail.com>
2024-04-17 19:46:08 -0700

    flake.nix: remove hypothesis from nativeCheckInputs

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
           pname = "ananke";
           format = "pyproject";
           buildInputs = with pkgs.python3Packages; [ setuptools ];
-          nativeCheckInputs = with pkgs; [ python3Packages.unittestCheckHook python3Packages.hypothesis gnupg ];
+          nativeCheckInputs = with pkgs; [ python3Packages.unittestCheckHook gnupg ];
           src = builtins.path {
             path = ./.;
             name = "ananke-py-src";
