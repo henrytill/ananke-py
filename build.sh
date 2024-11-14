8788565ea88f1c6c3672b5f489e11645015fc428
Henry Till <henrytill@gmail.com>
2024-11-14 15:26:19 -0800

    build.sh: rework

--- build.sh
+++ build.sh
@@ -1,97 +1,154 @@
 #!/usr/bin/env bash
 
+# Exit on error, undefined vars, and pipe failures
 set -o errexit
 set -o nounset
 set -o pipefail
 
-PYTHON=python3
-MYPY=mypy
-VENV=env
+# Configuration variables
+readonly PYTHON=python3
+readonly MYPY=mypy
+readonly VENV=env
+readonly PACKAGE_NAME="ananke"
+readonly TEST_DIR="tests"
 
+# Default values for command line options
 USE_VENV=0
 VERBOSE=0
 
-generate() {
-    local version="0.1.0"
-
-    if [ -n "$(command -v git)" -a -z "${1+x}" ]; then
-        local git_ref=$(git rev-parse --short HEAD)
-        version="${version}+${git_ref}"
-    elif [ -n "${1+x}" ]; then
-        version="${version}+${1}"
-    fi
-
+log() {
     if [ $VERBOSE -eq 1 ]; then
-        printf "version=%s\n" $version
+        printf "[INFO] %s\n" "$1"
+    fi
+}
+
+error() {
+    printf "[ERROR] %s\n" "$1" >&2
+    exit 1
+}
+
+generate_version() {
+    local base_version="0.1.0"
+    local version="${base_version}"
+    local git_ref="${1:-}"
+
+    if [ -z "$git_ref" ] && command -v git >/dev/null; then
+        git_ref=$(git rev-parse --short HEAD 2>/dev/null || true)
     fi
 
-    cat <<EOF >ananke/version.py
+    if [ -n "$git_ref" ]; then
+        version="${base_version}+${git_ref}"
+    fi
+
+    log "Generated version: ${version}"
+
+    cat <<EOF >"${PACKAGE_NAME}/version.py"
 """This module contains version information."""
 
 # This file is auto-generated, do not edit by hand
-__version__ = "$version"
+__version__ = "${version}"
 EOF
 }
 
-activate() {
-    if [ $USE_VENV -eq 1 ]; then
+activate_venv() {
+    local flag=${1:-0}
+    if [ $USE_VENV -eq 1 -o $flag -eq 1 ]; then
+        if [ ! -f "${VENV}/bin/activate" ]; then
+            error "Virtual environment not found. Run './build.sh create-env' first"
+        fi
         source "${VENV}/bin/activate"
+        log "Activated virtual environment at ${VENV}"
     fi
 }
 
 create_env() {
-    USE_VENV=1
-    generate
-    $PYTHON -m venv $VENV
-    activate
-    which $PYTHON
+    if [ -d "$VENV" ]; then
+        error "Environment directory ${VENV} already exists. Remove it first to create a fresh environment."
+    fi
+
+    log "Creating new virtual environment..."
+    generate_version
+    $PYTHON -m venv "$VENV"
+    activate_venv 1
+    log "Using Python: $(which $PYTHON)"
     $PYTHON -m pip install --upgrade pip
-    $PYTHON -m pip install -e .[test,dev]
+    $PYTHON -m pip install -e ".[types,test,dev]"
+    log "Environment created successfully"
 }
 
 check() {
-    activate
-    $MYPY --no-color-output ananke
-    $MYPY --no-color-output tests
+    log "Running type checks..."
+    activate_venv
+    $MYPY --no-color-output "$PACKAGE_NAME" "$TEST_DIR"
 }
 
 lint() {
-    activate
+    log "Running linters..."
+    activate_venv
     $PYTHON -m flake8 --config .flake8
-    $PYTHON -m pylint ananke tests
+    $PYTHON -m pylint "$PACKAGE_NAME" "$TEST_DIR"
 }
 
-fmt() {
-    activate
-    $PYTHON -m isort ananke tests
-    $PYTHON -m black ananke tests
+format() {
+    log "Formatting code..."
+    activate_venv
+    $PYTHON -m isort "$PACKAGE_NAME" "$TEST_DIR"
+    $PYTHON -m black "$PACKAGE_NAME" "$TEST_DIR"
 }
 
-test() {
-    activate
-    $PYTHON -m unittest discover -v -s tests
+run_tests() {
+    log "Running tests..."
+    activate_venv
+    $PYTHON -m unittest discover -v -s "$TEST_DIR"
     $PYTHON -m doctest -v ananke/data/common.py
     $PYTHON -m cram tests
 }
 
-action() {
-    subcommand=$1
+usage() {
+    cat >&2 <<EOF
+Usage: $0 [-e] [-v] <command>
+
+Options:
+    -e          Use virtual environment
+    -v          Verbose output
+
+Commands:
+    create-env  Create a new virtual environment
+    generate    Generate version file
+    check       Run type checks
+    lint        Run linters
+    fmt         Format code
+    test        Run tests
+EOF
+    exit 1
+}
+
+main() {
+    # Parse global options
+    while getopts "ev" opt; do
+        case $opt in
+            e) USE_VENV=1 ;;
+            v) VERBOSE=1 ;;
+            *) usage ;;
+        esac
+    done
+    shift $((OPTIND - 1))
+
+    [ $# -eq 0 ] && usage
+
+    local command=$1
     shift
 
-    case $subcommand in
-        default)
-            printf "Hello, world!\n"
-            ;;
+    case $command in
         generate)
-            local git_ref=
-            while getopts "g:" name; do
-                case $name in
-                    g)
-                        git_ref="$OPTARG"
-                        ;;
+            local git_ref=""
+            while getopts "g:" opt; do
+                case $opt in
+                    g) git_ref="$OPTARG" ;;
+                    *) usage ;;
                 esac
             done
-            generate $git_ref
+            generate_version "$git_ref"
             ;;
         create-env)
             create_env
@@ -103,38 +160,15 @@ action() {
             lint
             ;;
         fmt)
-            fmt
+            format
             ;;
         test)
-            test
+            run_tests
+            ;;
+        *)
+            usage
             ;;
     esac
-
-    exit 0
 }
 
-while getopts "ev" name; do
-    case $name in
-        e)
-            USE_VENV=1
-            ;;
-        v)
-            VERBOSE=1
-            ;;
-    esac
-done
-
-shift $(($OPTIND - 1))
-
-unset name
-unset OPTIND
-
-if [ $VERBOSE -eq 1 ]; then
-    printf "USE_VENV=%d\n" $USE_VENV
-fi
-
-if [ $# -eq 0 ]; then
-    action default
-else
-    action $*
-fi
+main "$@"
